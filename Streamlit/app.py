332541576cd6efd0ca157c3fdde0d540c78dcbe4
romeomanoela <romeomanoela18@gmail.com>
2025-03-27 20:46:17 +0300

    fastAPI & streamlit

---
+++ Streamlit/app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.title("Hello World")
+
+# dataFrame Exemple
+df = pd.DataFrame({
+    "name": ['John', 'Jane', 'Joe'],
+    "age": [20, 21, 22],
+    "city": ['New York', 'London', 'Paris']
+})
+st.write("Here's our first attempt at using data to create a table:")
+st.write(df)
+
+chart_data = pd.DataFrame(
+    np.random.randn(6, 3),
+    columns=['a', 'b', 'c']
+)
+
+print(chart_data)
+
+st.line_chart(chart_data)
---
+++ Streamlit/widgets.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+name: str = st.text_input("Name")
+
+age: int = st.slider("Age", 0, 100, 25)
+
+st.write(f"Age: {age}")
+
+options = ['Python', 'Java', 'C++', 'JavaScript']
+selected_option = st.selectbox("Select an option", options)
+st.write(f"Selected option: {selected_option}")
+
+df_file = st.file_uploader("Upload a file", type='csv')
+
+if df_file is not None:
+    df = pd.read_csv(df_file)
+    st.write(df)
+
+if name:
+    st.write(f"Hello {name}")
